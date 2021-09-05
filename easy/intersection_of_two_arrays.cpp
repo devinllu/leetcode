0dab026ea2c886effd467eb842152b0a4779654c
devinlu <ludevinl@sfu.ca>
2021-09-04 18:23:42 -0700

    changed to auto

--- easy/intersection_of_two_arrays.cpp
+++ easy/intersection_of_two_arrays.cpp
@@ -11,10 +11,10 @@ public:
         set<int> s;
         vector<int> results;
         
-        for (int i = 0; i < nums1.size(); i++) {
-            for (int j = 0; j < nums2.size(); j++) {
-                if (nums1[i] == nums2[j]) {
-                    s.insert(nums1[i]);
+        for (auto i : nums1) {
+            for (auto j : nums2) {
+                if (i == j) {
+                    s.insert(i);
                     break;
                 }
             }
