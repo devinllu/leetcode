f1b29790549b7faf7388ef250fe191f56839b4c8
Devin Lu <ludevinl@sfu.ca>
2021-10-16 01:14:39 -0400

    dfs

---
+++ medium/number_of_islands.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        counter = 0
+        
+        def dfs(r, c):
+            
+            if grid[r][c] == '0':
+                return
+            
+            grid[r][c] = '0'
+            
+            if (r - 1) >= 0:
+                dfs(r - 1, c)
+                    
+            if (r + 1) < len(grid):
+                dfs(r + 1, c)
+                    
+            if (c - 1) >= 0:
+                dfs(r, c - 1)
+                    
+            if (c + 1) < len(grid[0]):
+                dfs(r, c + 1)
+            
+
+                
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1':
+                    dfs(r, c)
+                    counter += 1
+        return counter
