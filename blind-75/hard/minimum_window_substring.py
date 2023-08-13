5ab8b991753445d7029697d502b16468e81bd5aa
Devin <devinllu@hotmail.com>
2023-08-12 22:03:29 -0700

    b75(hard): min window substr

--- blind-75/hard/minimum_window_substring.py
+++ blind-75/hard/minimum_window_substring.py
@@ -1,36 +1,45 @@
+class Solution:
+    
+    def minWindow(self, s: str, t: str) -> str:
 
-def minWindow(s: str, t: str) -> str:
+        def valid_substr(d):
+            return all(value >= 0 for value in d.values())
 
-    lowest = ""
-    res = ""
-    min_length = float('inf')
+        lowest = ""
+        dq = []
+        counter = {}
 
-    counter = {}
-    for c in t:
-        if c in counter.keys():
-            counter[c] += 1
-        else:
-            counter[c] = 1
+        for c in t:
+            if c in counter.keys():
+                counter[c] -= 1
+            else:
+                counter[c] = -1
+        
+        for i, c in enumerate(s):
+            if c in counter.keys():
+                counter[c] += 1
+                dq.append((c, i))
 
-    for i in range(len(s)):
-        d = counter.copy()
-        print(d)
-        for j in range(i, len(s)):
-            c = s[j]
-            lowest += c
+                if valid_substr(counter) and lowest == "":
+                
+                    first = dq[0][1]
+                    second = dq[len(dq) - 1][1] + 1
 
-            if c in d.keys():
-                if d[c] != 0:
-                    print(f'lowest: {lowest}, i: {i}')
-                    d[c] -= 1
-                    
-            
-            if all(val == 0 for val in d.values()):
-                if len(lowest) < min_length:
-                    min_length = len(lowest)
-                    res = lowest
-                lowest = ""
-                break
-    return res
+                    lowest = s[first:second]
+                while any(value > 0 for value in counter.values()) and valid_substr(counter):
+                    vals = dq.pop(0)
+                    counter[vals[0]] -= 1
+                
+                    first = dq[0][1]
+                    second = dq[len(dq) - 1][1] + 1
 
-print(minWindow("wegdtzwabazduwwdysdetrrctotpcepalxdewzezbfewbabbseinxbqqplitpxtcwwhuyntbtzxwzyaufihclztckdwccpeyonumbpnuonsnnsjscrvpsqsftohvfnvtbphcgxyumqjzltspmphefzjypsvugqqjhzlnylhkdqmolggxvneaopadivzqnpzurmhpxqcaiqruwztroxtcnvhxqgndyozpcigzykbiaucyvwrjvknifufxducbkbsmlanllpunlyohwfsssiazeixhebipfcdqdrcqiwftutcrbxjthlulvttcvdtaiwqlnsdvqkrngvghupcbcwnaqiclnvnvtfihylcqwvderjllannflchdklqxidvbjdijrnbpkftbqgpttcagghkqucpcgmfrqqajdbynitrbzgwukyaqhmibpzfxmkoeaqnftnvegohfudbgbbyiqglhhqevcszdkokdbhjjvqqrvrxyvvgldtuljygmsircydhalrlgjeyfvxdstmfyhzjrxsfpcytabdcmwqvhuvmpssingpmnpvgmpletjzunewbamwiirwymqizwxlmojsbaehupiocnmenbcxjwujimthjtvvhenkettylcoppdveeycpuybekulvpgqzmgjrbdrmficwlxarxegrejvrejmvrfuenexojqdqyfmjeoacvjvzsrqycfuvmozzuypfpsvnzjxeazgvibubunzyuvugmvhguyojrlysvxwxxesfioiebidxdzfpumyon", "ozgzyywxvtublcl"))
+                    potential_lowest = s[first:second]
+                    if len(lowest) > len(potential_lowest) and valid_substr(counter):
+                        lowest = potential_lowest
+        return lowest
+
+# expected: "abbbbbcdd"
+# s = Solution()
+# first, second = 'aaaaaaaaaaaabbbbbcdd', 'abcdd'
+# print(f's: {first}, t: {second}\n')
+# print(s.minWindow(first, second))
