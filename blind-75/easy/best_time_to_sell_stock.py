0ae0230bdb9678635655ef441210b2f0c5635327
Devin <devinllu@hotmail.com>
2023-09-29 11:31:14 -0700

    b75(easy): refactors

--- blind-75/easy/best_time_to_sell_stock.py
+++ blind-75/easy/best_time_to_sell_stock.py
@@ -1,3 +1,18 @@
+'''
+description: You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+
+buy on day 2, sell on day 5, hence max profit = 5
+
+takes advantage of greedy solution, where you want to find min_price as you iterate, then check for profit
+'''
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if len(prices) == 1:
--- blind-75/easy/climbing_stairs.py
+++ blind-75/easy/climbing_stairs.py
@@ -1,3 +1,18 @@
+'''
+description: You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+Input: n = 2
+Output: 2
+
+1. 1 step + 1 step
+2. 2 steps
+
+need to update first + second vars as you go, memoize
+'''
+
 class Solution:
     def climbStairs(self, n: int) -> int:
         # 1 1 1 1 1
